2bbf05f023c70a1a1d6892a00174a531f7141d32
JoshCantor <josh.s.cantor@gmail.com>
2015-11-04 13:32:38 -0800

    built red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your JS goes here
+var Board = function (numberOfTiles) {
+	var board = [];
+	for (i = 1; i < numberOfTiles; i++) {
+		var color = "red";
+		if (i % 2 !== 0) {
+			color = "black";
+		}
+		var tile = new Tile(i, color);
+		board.push(tile);
+	}
+	this.board = board;
+}
+
+var Tile = function (name, color) {
+	var name = document.createElement("div");
+	document.body.appendChild(name);
+	name.style.width = "11.1%";
+	name.style.paddingBottom = "11.1%"
+	name.style.float = "left";
+	name.style.background = color
+	
+}
+
+var board = new Board(64);
+console.log(board);
